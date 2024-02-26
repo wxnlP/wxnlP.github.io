a2e1cde98a74d73d32ccc97cb357c448fa9d4cf3
wxnlP <2661006892@qq.com>
2024-02-26 14:02:11 +0800

    Site updated: 2024-02-26 14:02:07

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,1 +1,17 @@
-var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png"),document.title="我相信你还会回来的！",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png"),document.title="哈哈，我就知道！"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
+//
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        document.title = '我相信你还会回来的！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        document.title = '哈哈，我就知道！' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
